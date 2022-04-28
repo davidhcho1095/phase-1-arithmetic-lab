57bdd6376d7171db228dada9c4e784b5eaeaf71c
David Cho <davidhcho1095@gmail.com>
2022-04-28 18:27:51 -0400

    commit tree 4c9e1d5a0053410ee5c3f9c99902dc8f98ce0c31

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+//multiplication equation
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+//random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+//mod equation
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4
+//max will return 20 as highest number
+let max = Math.max(1,2,3,4,5,6,7,20)
